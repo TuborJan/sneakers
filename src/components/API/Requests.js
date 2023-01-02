c616414c82a48f1fb7137223f99092d55f73c618
Дмитрий Калита <TuborJan@yandex.ru>
2023-01-02 18:40:20 +0300

    feat: add adaptive layout

--- src/components/API/Requests.js
+++ src/components/API/Requests.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-export async function postData(to, item) {
+export const postData = async (to, item) => {
   await axios
     .post(`https://63a57287318b23efa793b328.mockapi.io/${to}`, item)
     .catch((error) => {
       alert(error.message);
     });
-}
+};
 
-export async function deleteData(from, item) {
+export const deleteData = async (from, item) => {
   await axios
     .delete(`https://63a57287318b23efa793b328.mockapi.io/${from}/${item}`)
     .catch((error) => {
       alert(error.message);
     });
-}
+};
 
-export async function fetchItemsData(from, to, setError) {
+export const fetchItemsData = async (from, to, setError) => {
   const response = await axios
     .get(`https://63a57287318b23efa793b328.mockapi.io/${from}`)
     .catch((error) => {
       setError(error.message);
     });
   to(response.data);
-}
+};
