3ccea33e9085a937afb54814e2041e653bc3ee5a
TuborJan <TuborJan@yandex.ru>
2023-05-25 14:56:19 +0300

    refactor

--- src/Service/API/Requests.js
+++ src/Service/API/Requests.js
@@ -1,22 +1,6 @@
 import axios from "axios";
 import { setProductsCard } from "../Store/store";
 
-export const postData = async (to, item) => {
-  await axios
-    .post(`https://63a57287318b23efa793b328.mockapi.io/${to}`, item)
-    .catch((error) => {
-      alert(error.message);
-    });
-};
-
-export const deleteData = async (from, item) => {
-  await axios
-    .delete(`https://63a57287318b23efa793b328.mockapi.io/${from}/${item}`)
-    .catch((error) => {
-      alert(error.message);
-    });
-};
-
 export const getItemsData = (from, setError, setIsLoading) => {
   const response = axios
     .get(`https://63a57287318b23efa793b328.mockapi.io/${from}`)
